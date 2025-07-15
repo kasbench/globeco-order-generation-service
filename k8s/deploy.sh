ea232436fdf3595d45ca5b1aba497ec003259ab1
Noah Krieger <noahkrieger@gmail.com>
2025-07-15 09:25:46 -0400

    updated deployment script

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -3,6 +3,7 @@ set -e
 
 NAMESPACE=globeco
 MONGODB_STS=globeco-order-generation-service-mongodb
+REDIS_STS=globeco-order-generation-service-redis
 
 # Deploy MongDB StatefulSet and Service
 kubectl apply -f k8s/mongodb-deployment.yaml
@@ -14,7 +15,19 @@ until kubectl -n "$NAMESPACE" get statefulset "$MONGODB_STS" -o jsonpath='{.stat
   sleep 5
 done
 
-echo "MongDB StatefulSet is ready. Deploying Order Generation Service."
+echo "MongoDB StatefulSet is ready. Deploying Redis."
+
+# Deploy Redis StatefulSet and Service
+kubectl apply -f k8s/redis.yaml
+
+echo "Waiting for Redis StatefulSet to be ready..."
+# Wait for the Redis StatefulSet to have at least 1 ready replica
+until kubectl -n "$NAMESPACE" get statefulset "$REDIS_STS" -o jsonpath='{.status.readyReplicas}' | grep -q '^1$'; do
+  echo "  ...still waiting for Redis to be ready..."
+  sleep 5
+done
+
+echo "Redis StatefulSet is ready. Deploying Order Generation Service."
 
 # Deploy application Deployment and Service
 kubectl apply -f k8s/deployment.yaml
